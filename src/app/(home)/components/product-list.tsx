142308abc6eee5d84ee7a139d354cf70585886ac
Sayam Alvi <sayamalvi07@gmail.com>
2024-12-31 23:23:31 +0530

    add component streaming

---
+++ src/app/(home)/components/product-list.tsx
@@ -0,0 +1,80 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Category, Product } from "@/lib/types";
+import ProductCard from "./product-card";
+
+const ProductList = async () => {
+    const categoryResponse = await fetch(`${process.env.BACKEND_URL}/api/catalog/categories`, {
+        next: {
+            revalidate: 3600,
+        },
+    })
+    if (!categoryResponse.ok) {
+        throw new Error('Failed to fetch categories')
+    }
+    const categories: Category[] = await categoryResponse.json()
+
+    const productsResponse = await fetch(
+        `${process.env.BACKEND_URL}/api/catalog/products?tenantId=4`,
+        {
+            next: {
+                revalidate: 3600,
+
+            },
+        }
+    );
+
+    if (!productsResponse.ok) {
+        throw new Error("Failed to fetch products");
+    }
+    const { products }: { products: Product[] } = await productsResponse.json();
+
+    return (
+        <section className="p-[5rem]">
+            <div className="container py-12">
+                <Tabs defaultValue={categories[0]._id} className="">
+                    <TabsList>
+                        {categories.map((category) => {
+                            return (
+                                <TabsTrigger
+                                    key={category._id}
+                                    value={category._id}
+                                    className="text-md">
+                                    {category.name}
+                                </TabsTrigger>
+                            );
+                        })}
+                    </TabsList>
+                    {categories.map((category) => {
+                        return (
+                            <TabsContent key={category._id} value={category._id}>
+                                <div className="grid grid-cols-4 gap-6 mt-6">
+                                    {products
+                                        .filter(
+                                            (product) => product.category._id === category._id
+                                        )
+                                        .map((product) => (
+                                            <ProductCard product={product} key={product._id} />
+                                        ))}
+                                </div>
+                            </TabsContent>
+                        );
+                    })}
+                    <TabsContent value="pizza">
+                        <div className="grid grid-cols-4 gap-6 mt-6">
+                            {products.map((product: Product) => {
+                                return (
+                                    <ProductCard key={product._id} product={product} />
+                                )
+                            })}
+                        </div>
+                    </TabsContent>
+                    <TabsContent value="beverages">
+                        beverages List
+                    </TabsContent>
+                </Tabs>
+            </div>
+        </section>
+    )
+}
+
+export default ProductList
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,36 +1,9 @@
 import { Button } from "@/components/ui/button";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Image from "next/image";
-import ProductCard from "./components/product-card";
-import type { Category, Product } from "@/lib/types";
-
-const Home = async () => {
-  // TODO: Do concurrent requests -> Promise.all
-  const categoryResponse = await fetch(`${process.env.BACKEND_URL}/api/catalog/categories`, {
-    next: {
-      revalidate: 3600,
-    },
-  })
-  if (!categoryResponse.ok) {
-    throw new Error('Failed to fetch categories')
-  }
-  const categories: Category[] = await categoryResponse.json()
-
-  const productsResponse = await fetch(
-    `${process.env.BACKEND_URL}/api/catalog/products?tenantId=4`,
-    {
-      next: {
-        revalidate: 3600,
-
-      },
-    }
-  );
-
-  if (!productsResponse.ok) {
-    throw new Error("Failed to fetch products");
-  }
-  const { products }: { products: Product[] } = await productsResponse.json();
-
+import ProductList from "./components/product-list";
+import { Suspense } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+const Home = () => {
   return (
     <>
       <section className="bg-white p-[5rem]">
@@ -52,53 +25,10 @@ const Home = async () => {
           </div>
         </div>
       </section>
-      <section className="p-[5rem]">
-        <div className="container py-12">
-          <Tabs defaultValue={categories[0]._id} className="">
-            <TabsList>
-              {categories.map((category) => {
-                return (
-                  <TabsTrigger
-                    key={category._id}
-                    value={category._id}
-                    className="text-md">
-                    {category.name}
-                  </TabsTrigger>
-                );
-              })}
-            </TabsList>
-            {categories.map((category) => {
-              return (
-                <TabsContent key={category._id} value={category._id}>
-                  <div className="grid grid-cols-4 gap-6 mt-6">
-                    {products
-                      .filter(
-                        (product) => product.category._id === category._id
-                      )
-                      .map((product) => (
-                        <ProductCard product={product} key={product._id} />
-                      ))}
-                  </div>
-                </TabsContent>
-              );
-            })}
-            <TabsContent value="pizza">
-              <div className="grid grid-cols-4 gap-6 mt-6">
-                {products.map((product: Product) => {
-                  return (
-                    <ProductCard key={product._id} product={product} />
-                  )
-                })}
-              </div>
-            </TabsContent>
-            <TabsContent value="beverages">
-              beverages List
-            </TabsContent>
-          </Tabs>
-        </div>
-      </section>
+      <Suspense fallback={<Skeleton className="w-[100px] h-[20px] rounded-full" />}>
+        <ProductList />
+      </Suspense>
     </>
-
   );
 }
 
