54b30edf1084abe18e7d992f0fbf9979aef71afd
Sayam Alvi <sayamalvi07@gmail.com>
2025-01-08 16:02:48 +0530

    order summary calculation

--- src/app/checkout/components/customer-form.tsx
+++ src/app/checkout/components/customer-form.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { z } from 'zod'
-import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
@@ -13,8 +12,8 @@ import { Coins, CreditCard } from 'lucide-react';
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { FormControl, FormField, FormItem, Form, FormMessage } from '@/components/ui/form';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
 import AddAddress from './add-address';
+import OrderSummary from './order-summary';
 
 const customerFormSchema = z.object({
     address: z.string({ required_error: 'Please select an address' }),
@@ -155,47 +154,7 @@ const CustomerForm = () => {
                             </div>
                         </CardContent>
                     </Card>
-                    <Card className="w-2/5 border-none h-auto self-start">
-                        <CardHeader>
-                            <CardTitle>Order summary</CardTitle>
-                        </CardHeader>
-                        <CardContent className="grid gap-4 pt-6">
-                            <div className="flex items-center justify-between">
-                                <span>Subtotal</span>
-                                <span className="font-bold">₹8130</span>
-                            </div>
-                            <div className="flex items-center justify-between">
-                                <span>Taxes</span>
-                                <span className="font-bold">₹82</span>
-                            </div>
-                            <div className="flex items-center justify-between">
-                                <span>Delivery charges</span>
-                                <span className="font-bold">₹100</span>
-                            </div>
-                            <div className="flex items-center justify-between">
-                                <span>Discount</span>
-                                <span className="font-bold">₹0</span>
-                            </div>
-                            <hr />
-                            <div className="flex items-center justify-between">
-                                <span className="font-bold">Order total</span>
-                                <span className="font-bold">₹8300</span>
-                            </div>
-                            <div className="flex items-center gap-4">
-                                <Input
-                                    id="fname"
-                                    type="text"
-                                    className="w-full"
-                                    placeholder="Coupon code"
-                                />
-                                <Button variant={'outline'}>Apply</Button>
-                            </div>
-
-                            <div className="text-right mt-6">
-                                <Button>Place order</Button>
-                            </div>
-                        </CardContent>
-                    </Card>
+                    <OrderSummary />
                 </div>
             </form>
         </Form>
---
+++ src/app/checkout/components/order-summary.tsx
@@ -0,0 +1,81 @@
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { useAppSelector } from '@/lib/store/hooks'
+import { getItemTotal } from '@/lib/utils'
+import React, { useMemo, useState } from 'react'
+
+// todo: move these to backend
+const TAXES_PERCENTAGE = 18
+const DELIVERY_CHARGES = 100
+
+const OrderSummary = () => {
+    const cart = useAppSelector((state) => state.cart.cartItems)
+    const [discountPercentage, setDiscountPercentage] = useState(0)
+
+    const subTotal = useMemo(() => {
+        return cart.reduce((acc, curr) => {
+            return acc + curr.qty * getItemTotal(curr)
+        }, 0)
+    }, [cart])
+
+
+    const discountAmount = useMemo(() => {
+        return Math.round((subTotal * discountPercentage) / 100)
+    }, [subTotal])
+
+    const taxesAmount = useMemo(() => {
+        const amountAfterDiscount = subTotal - discountAmount
+        return Math.round((amountAfterDiscount * TAXES_PERCENTAGE) / 100)
+    }, [])
+
+    const grandTotal = useMemo(() => {
+        return subTotal - discountAmount + taxesAmount + DELIVERY_CHARGES
+    }, [subTotal, discountAmount, taxesAmount])
+
+    return (
+        <Card className="w-2/5 border-none h-auto self-start">
+            <CardHeader>
+                <CardTitle>Order summary</CardTitle>
+            </CardHeader>
+            <CardContent className="grid gap-4 pt-6">
+                <div className="flex items-center justify-between">
+                    <span>Subtotal</span>
+                    <span className="font-bold">₹{subTotal}</span>
+                </div>
+                <div className="flex items-center justify-between">
+                    <span>Taxes</span>
+                    <span className="font-bold">₹{taxesAmount}</span>
+                </div>
+                <div className="flex items-center justify-between">
+                    <span>Delivery charges</span>
+                    <span className="font-bold">₹{DELIVERY_CHARGES}</span>
+                </div>
+                <div className="flex items-center justify-between">
+                    <span>Discount</span>
+                    <span className="font-bold">₹{discountAmount}</span>
+                </div>
+                <hr />
+                <div className="flex items-center justify-between">
+                    <span className="font-bold">Order total</span>
+                    <span className="font-bold">₹{grandTotal}</span>
+                </div>
+                <div className="flex items-center gap-4">
+                    <Input
+                        id="fname"
+                        type="text"
+                        className="w-full"
+                        placeholder="Coupon code"
+                    />
+                    <Button variant={'outline'}>Apply</Button>
+                </div>
+
+                <div className="text-right mt-6">
+                    <Button>Place order</Button>
+                </div>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default OrderSummary
